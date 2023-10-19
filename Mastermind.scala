91c20bd4db18fb065209e2092d4bc916667b5590
imranjan75 <janimran7599@gmail.com>
2023-10-19 13:21:45 +0200

    Main-Klasse wurde angepasst

--- Mastermind.scala
+++ Mastermind.scala
@@ -1,52 +1,10 @@
 package projekt
 
+import MasterMind.MastermindGame
+
 object Mastermind {
   def main(args: Array[String]): Unit = {
-    import scala.io.StdIn.readLine
-    import scala.util.Random
-
-    // Dein String-Array mit 6 Elementen
-    val meinArray = Array("R", "G", "B", "W", "S")
-
-    // Die Anzahl der Elemente, die du zufällig auswählen möchtest
-    val anzahlAuswahl = 3
-
-    // Eine leere Liste, um die ausgewählten Elemente zu speichern
-    var zufaelligeFarben = List[String]()
-
-    // Verwende eine Schleife, um zufällige Indizes zu generieren und die Elemente auszuwählen
-    while (zufaelligeFarben.length < anzahlAuswahl) {
-      val randomIndex = Random.nextInt(meinArray.length)
-      val eineFarbe = meinArray(randomIndex)
-      if (!zufaelligeFarben.contains(eineFarbe)) {
-        zufaelligeFarben = zufaelligeFarben :+ eineFarbe
-      }
+    MastermindGame(4).startGame()
     }
-
-    // Die ausgewählten Elemente anzeigen
-    zufaelligeFarben.foreach(print)
-
-    println()
-
-    var versuchAnzahl = 0
-
-    while (versuchAnzahl < 10){
-      var eingabe = readLine("Geben Sie R, G, B, W, S: ").split(" ")
-
-      for (z <- zufaelligeFarben){
-
-        if (eingabe.contains(z))
-          {
-            if (zufaelligeFarben.indexOf(z) == eingabe.indexOf(z)) println("Hit")
-
-          }
-
-      }
-    }
-    }
-
-
-
-
 
 }
