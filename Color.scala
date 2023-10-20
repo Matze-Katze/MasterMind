c9ffd80acbb2d5c3e5cfd162f0f836f631d5d2a2
imranjan75 <janimran7599@gmail.com>
2023-10-20 07:58:54 +0200

    In der Color-Klasse werden die Farben aufgezählt

--- Color.scala
+++ Color.scala
@@ -4,8 +4,7 @@ package MasterMind
 import scala.util.Random
 
 
-protected object Color extends Enumeration
-{
+protected object Color extends Enumeration {
   type Color = Value
   val Red, Blue , Green, Yellow, Orange = Value
 }
