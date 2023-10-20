83e6f17678d9a1e949d4f80d9a191065c5cf9e9e
imranjan75 <janimran7599@gmail.com>
2023-10-20 07:57:55 +0200

    Mastermind und MastermindGame wurden angepasst

--- Color.scala
+++ Color.scala
@@ -4,7 +4,8 @@ package MasterMind
 import scala.util.Random
 
 
-protected object Color extends Enumeration{
+protected object Color extends Enumeration
+{
   type Color = Value
   val Red, Blue , Green, Yellow, Orange = Value
 }
