d2b4122e682215680f4f11b348e72aa989c50464
Matze-Katze <Matthias.leray@htwg-konstanz.de>
2023-11-03 11:26:11 +0100

    test

--- src/main/scala/Mastermindgame/Color.scala
+++ src/main/scala/Mastermindgame/Color.scala
@@ -2,7 +2,7 @@ package Mastermindgame
 
 protected object Color extends Enumeration {
   type Color = Value
-  val Red, Blue , Green, Yellow, Orange = Value
+  val Red, Blue , Green, Yellow, Orange = Value  
 
 }
 
